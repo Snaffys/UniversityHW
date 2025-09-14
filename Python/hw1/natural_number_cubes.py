e3d640e2b601371ed277f6e81cebb40c131ce2c8
Snaffys <grezindanil@gmail.com>
2025-09-14 22:45:42 +0300

    first Python homework - cube of natural number + bytecode

---
+++ Python/hw1/natural_number_cubes.py
@@ -0,0 +1,16 @@
+def cube_of_natural_number(value):
+    try:
+        num = int(value)
+        if(num > 0):
+            return num ** 3
+        else:
+            return "undefined. Not a natural number"
+    except(ValueError, TypeError):
+        return "undefined. Not a natural number"
+
+def main():
+    input_value = input("Write down natural number: ")
+    print("Cube of this number:", cube_of_natural_number(input_value))
+    
+if __name__ == "__main__":
+    main()
